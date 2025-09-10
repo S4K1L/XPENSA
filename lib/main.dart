4bbed810c017547113b018639aa84651bd5a2c4c
SiRUSsir <69shakilmahmud@gmail.com>
2025-09-10 21:29:29 +0600

    CI/CD Pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,6 @@ import 'firebase_options.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await NotificationService.init();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
